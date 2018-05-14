2021995e2f02bc3cfc3b348e5e304cdc6a059641
mozartk-mac <mozartk30@gmail.com>
2018-05-14 21:46:47 +0900

    make basic test

--- tests/ProcessFinderTest.php
+++ tests/ProcessFinderTest.php
@@ -3,8 +3,20 @@
 namespace mozartk\ProcessChecker\Test;
 
 use mozartk\ProcessFinder\ProcessFinder;
+use Symfony\Component\Process\Process;
 use PHPUnit\Framework\TestCase;
 
 class ProcessCheckerTest extends TestCase
 {
+    public function testGetProcess()
+    {
+
+        $processFinder = new ProcessFinder();
+        $process = new Process('wait 5');
+        $process->start();
+        $pid = $process->getPid();
+
+        $process = $processFinder->getProcess($pid);
+        $this->assertEquals($pid, $process->getPid());
+    }
 }
